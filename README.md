0b1f9a1ab8946afb45900027915ba582bc42dbe8
Alexander Strack <strack.as@icloud.com>
2023-06-14 12:32:28 +0200

    import hpx

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hpx_fft
+
+export HPXSC_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/../hpxsc_installations/hpx_dist_v1.8.1"
+export CMAKE_COMMAND=${HPXSC_ROOT}/build/cmake/bin/cmake
+export HPX_DIR=${HPXSC_ROOT}/build/hpx/build/lib
+
+rm -rf build && mkdir build && cd build 
+
+$CMAKE_COMMAND .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="${HPX_DIR}/cmake/HPX"
+
+make all
--- src/sender_receiver.cpp
+++ src/sender_receiver.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <hpx/init.hpp>
 
 int hpx_main(hpx::program_options::variables_map& vm)
 {
@@ -8,10 +10,10 @@ int hpx_main(hpx::program_options::variables_map& vm)
 int main(int argc, char* argv[])
 {
     hpx::program_options::options_description desc_commandline;
-    hpx::local::init_params init_args;
+    hpx::init_params init_args;
     // Setup input arguments
     desc_commandline.add_options();
     // Run HPX main
     init_args.desc_cmdline = desc_commandline;
-    return hpx::local::init(hpx_main, argc, argv, init_args);
+    return hpx::init(argc, argv, init_args);
 }
