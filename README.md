bb29a540e4ab62c03b0def302c855e1f411bc518
Alexander Strack <strack.as@icloud.com>
2023-06-14 19:42:47 +0200

    test alternativ fftw include

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ export CMAKE_COMMAND=${HPXSC_ROOT}/build/cmake/bin/cmake
 export HPX_DIR=${HPXSC_ROOT}/build/hpx/build/lib
 
 export PKG_CONFIG_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/../fft_installations/fftw/install/lib"
+
 rm -rf build && mkdir build && cd build 
 $CMAKE_COMMAND .. -DCMAKE_BUILD_TYPE=Release -DHPX_DIR="${HPX_DIR}/cmake/HPX" -DPKG_CONFIG_PATH=${PKG_CONFIG_PATH} 
 
--- src/test_fftw.cpp
+++ src/test_fftw.cpp
@@ -6,6 +6,74 @@
 
 #include <fftw3.h>
 
+class FFTW_Adapter {
+	    shared_ptr<Global> global;
+	    Utilities utilities;
+
+	    //plans for FFF basis
+		FFTW_PLAN fft_plan_Fourier_forward_x;
+        FFTW_PLAN fft_plan_Fourier_inverse_x;
+        FFTW_PLAN fft_plan_Fourier_forward_y;
+        FFTW_PLAN fft_plan_Fourier_inverse_y;
+        FFTW_PLAN fft_plan_Fourier_forward_z;
+        FFTW_PLAN fft_plan_Fourier_inverse_z;
+
+        //plans for sine/ cosine basis
+        FFTW_PLAN fft_plan_Sine_forward_x;
+		FFTW_PLAN fft_plan_Sine_inverse_x;
+		FFTW_PLAN fft_plan_Sine_forward_y;
+		FFTW_PLAN fft_plan_Sine_inverse_y;
+		FFTW_PLAN fft_plan_Sine_forward_z;
+		FFTW_PLAN fft_plan_Sine_inverse_z;
+
+		FFTW_PLAN fft_plan_Cosine_forward_x;
+		FFTW_PLAN fft_plan_Cosine_inverse_x;
+		FFTW_PLAN fft_plan_Cosine_forward_y;
+		FFTW_PLAN fft_plan_Cosine_inverse_y;
+		FFTW_PLAN fft_plan_Cosine_forward_z;
+		FFTW_PLAN fft_plan_Cosine_inverse_z;
+
+        Array<Complex,3> FA;
+        Array<Complex,3> IA;
+        Array<Real,3> RA;
+
+	public: 
+		FFTW_Adapter();
+		FFTW_Adapter(shared_ptr<Global> global);
+		void init_transform(string basis);
+		void DFT_forward_x(Array<Complex,2> A);
+		void DFT_forward_R2C(Array<Real,2> Ar);
+		void DFT_inverse_x(Array<Complex,2> A);
+		void DFT_inverse_C2R(Array<Real,2> Ar);
+
+		void DFT_forward_x(Array<Complex,3> A);
+		void DFT_forward_R2C(Array<Real,3> Ar);
+		void DFT_forward_y(Array<Complex,3> IA);
+		void DFT_inverse_x(Array<Complex,3> A);
+		void DFT_inverse_C2R(Array<Real,3> Ar);
+		void DFT_inverse_y(Array<Complex,3> IA);
+
+		void DFT_forward_R2R(string basis_option, Array<Real,2> Ar);
+		void DFT_forward_R2R_x(string basis_option, Array<Complex,2> A);
+		void DFT_inverse_R2R(string basis_option, Array<Real,2> Ar);
+		void DFT_inverse_R2R_x(string basis_option, Array<Complex,2> A);
+
+		void DFT_forward_R2R(string basis_option, Array<Real,3> Ar);
+		void DFT_forward_R2R_x(string basis_option, Array<Complex,3> A);
+		void DFT_inverse_R2R(string basis_option, Array<Real,3> Ar);
+		void DFT_inverse_R2R_x(string basis_option, Array<Complex,3> A);
+		void DFT_forward_R2R_y(string basis_option, Array<Complex,3> IA);
+		void DFT_inverse_R2R_y(string basis_option, Array<Complex,3> IA);
+
+		void DFT_forward_C2C(string basis_option, Array<Complex,2> A);
+		void DFT_inverse_C2C(string basis_option, Array<Complex,2> A);
+		void DFT_forward_C2C(string basis_option, Array<Complex,3> A);
+		void DFT_inverse_C2C(string basis_option, Array<Complex,3> A);
+
+
+		void Finalize();
+	};
+    
 int hpx_main(hpx::program_options::variables_map& vm)
 {
   std::cout << "Hello World\n";
