33586feabe327f37fee362ecbc1631f5cd8fd746
Alexander Strack <strack.as@icloud.com>
2023-06-15 11:35:37 +0200

    try std complex

--- src/test_fftw.cpp
+++ src/test_fftw.cpp
@@ -3,6 +3,7 @@
 #include <hpx/hpx.hpp>
 #include <hpx/init.hpp>
 #include <hpx/future.hpp>
+#include <complex>
 
 #include <fftw3.h>
 
@@ -15,42 +16,49 @@
 #define REAL 0
 #define IMAG 1
 
-void acquire_from_somewhere(fftw_complex* signal) {
+void acquire_from_somewhere(std::vector<std::complex<double>>& signal) {
   /* Generate two sine waves of different frequencies and
   * amplitudes.
   */
-
   int i;
   for (i = 0; i < NUM_POINTS; ++i) {
     double theta = (double)i / (double)NUM_POINTS * M_PI;
 
-    signal[i][REAL] = i;// 1.0 * cos(10.0 * theta) + 0.5 * cos(25.0 * theta);
+    //signal[i][REAL] = i;// 1.0 * cos(10.0 * theta) + 0.5 * cos(25.0 * theta);
 
-    signal[i][IMAG] = NUM_POINTS - 1 - i;// 1.0 * sin(10.0 * theta) + 0.5 * sin(25.0 * theta);
+    //signal[i][IMAG] = NUM_POINTS - 1 - i;// 1.0 * sin(10.0 * theta) + 0.5 * sin(25.0 * theta);
+
+    signal[i].real(i);
+    signal[i].imag(NUM_POINTS-1-i);
   }
 }
 
-void do_something_with(fftw_complex* result) {
+void do_something_with(std::vector<std::complex<double>>& result) {
   int i;
   for (i = 0; i < NUM_POINTS; ++i) {
-    double mag = sqrt(result[i][REAL] * result[i][REAL] +
-    result[i][IMAG] * result[i][IMAG]);
-
-    printf("%g - %g\n", result[i][REAL], result[i][IMAG]);
+    // double mag = sqrt(result[i][REAL] * result[i][REAL] +
+    // result[i][IMAG] * result[i][IMAG]);
+    printf("%g - %g\n", result[i].real(), result[i].imag());
+    //printf("%g - %g\n", result[i][REAL], result[i][IMAG]);
   }
 }
 
 int hpx_main(hpx::program_options::variables_map& vm)
 {
-      fftw_complex signal[NUM_POINTS];
-  fftw_complex result[NUM_POINTS];
+  std::vector<std::complex<double>> signal;
+  std::vector<std::complex<double>> result;
+  signal.resize(NUM_POINTS);
+  result.resize(NUM_POINTS);
+  //fftw_complex signal[NUM_POINTS];
+  //fftw_complex result[NUM_POINTS];
 
   fftw_plan plan = fftw_plan_dft_1d(NUM_POINTS,
-  signal,
-  result,
-  FFTW_FORWARD,
-  FFTW_ESTIMATE);
+            reinterpret_cast<fftw_complex*>(&signal[0]),
+            reinterpret_cast<fftw_complex*>(&result[0]),
+            FFTW_FORWARD,
+            FFTW_ESTIMATE);
 
+  //acquire_from_somewhere(signal);
   acquire_from_somewhere(signal);
   fftw_execute(plan);
   do_something_with(result);
