f88f4f81c0ab405ba3835cb30835d8d379e854fe
Alexander Strack <strack.as@icloud.com>
2023-08-16 15:51:19 +0200

    test all to all

--- src/all_to_all.cpp
+++ src/all_to_all.cpp
@@ -23,108 +23,103 @@ using namespace hpx::collectives;
 
 constexpr char const* all_to_all_direct_basename = "/test/all_to_all_direct/";
 
-void test_one_shot_use()
-{
-    std::uint32_t this_locality = hpx::get_locality_id();
-    std::uint32_t num_localities = hpx::get_num_localities(hpx::launch::sync);
+// void test_multiple_use_with_generation()
+// {
+//     std::uint32_t num_localities = hpx::get_num_localities(hpx::launch::sync);
+//     std::uint32_t this_locality = hpx::get_locality_id();
 
-    // test functionality based on immediate local result value
-    for (int i = 0; i != 100; ++i)
-    {
-        std::vector<std::uint32_t> values(num_localities);
-        std::fill(values.begin(), values.end(), this_locality);
+//     auto all_to_all_direct_client =
+//         create_communicator(all_to_all_direct_basename,
+//             num_sites_arg(num_localities), this_site_arg(this_locality));
 
-        hpx::future<std::vector<std::uint32_t>> overall_result =
-            all_to_all(all_to_all_direct_basename, std::move(values),
-                num_sites_arg(num_localities), this_site_arg(this_locality),
-                generation_arg(i + 1));
 
-        std::vector<std::uint32_t> r = overall_result.get();
-        HPX_TEST_EQ(r.size(), num_localities);
+//     // test functionality based on immediate local result value
+//     for (int i = 0; i != 1; ++i)
+//     {
+//         std::vector<std::vector<std::uint32_t>> values(num_localities);
+        
+//         for (std::size_t j = 0; j != num_localities; ++j)
+//         {
+//             values[j].resize(3);
+//             std::fill(values[j].begin(), values[j].end(), this_locality);
+//         }
 
-        for (std::size_t j = 0; j != r.size(); ++j)
-        {
-            HPX_TEST_EQ(r[j], j);
-        }
-        // if(this_locality == 0)
-        // {
-        //     for (auto v : r)
-        //     {
-        //     char const* msg = "Locality {1}: {2}\n";
-        //     hpx::util::format_to(hpx::cout, msg, this_locality, v)
-        //         << std::flush;
-        //     }
-        // }
-    }
-}
 
-void test_multiple_use()
-{
-    std::uint32_t num_localities = hpx::get_num_localities(hpx::launch::sync);
-    std::uint32_t this_locality = hpx::get_locality_id();
+//         hpx::future<std::vector<std::vector<std::uint32_t>>> overall_result = all_to_all(
+//             all_to_all_direct_client, std::move(values), generation_arg(i + 1));
 
-    auto all_to_all_direct_client =
-        create_communicator(all_to_all_direct_basename,
-            num_sites_arg(num_localities), this_site_arg(this_locality));
+//         std::vector<std::vector<std::uint32_t>> r = overall_result.get();
 
-    // test functionality based on immediate local result value
-    for (int i = 0; i != 100; ++i)
-    {
-        std::vector<std::uint32_t> values(num_localities);
-        std::fill(values.begin(), values.end(), this_locality);
-
-        hpx::future<std::vector<std::uint32_t>> overall_result =
-            all_to_all(all_to_all_direct_client, std::move(values));
-
-        std::vector<std::uint32_t> r = overall_result.get();
-        HPX_TEST_EQ(r.size(), num_localities);
-
-        for (std::size_t j = 0; j != r.size(); ++j)
-        {
-            HPX_TEST_EQ(r[j], j);
-        }
-    }
-}
+//         if(1)//this_locality == 0)
+//         {
+//             for (std::size_t j = 0; j != num_localities; ++j)
+//             {
+//                 for (auto v : r[j])
+//                 {
+//                 char const* msg = "Locality {1} r: {2}\n";
+//                 hpx::util::format_to(hpx::cout, msg, this_locality, v)
+//                     << std::flush;
+//                 }
+//             }
+//         }
+//     }
+// }
 
 void test_multiple_use_with_generation()
 {
     std::uint32_t num_localities = hpx::get_num_localities(hpx::launch::sync);
     std::uint32_t this_locality = hpx::get_locality_id();
 
+    std::uint32_t size=4;
+    std::uint32_t sub_size=size/num_localities;
+
     auto all_to_all_direct_client =
         create_communicator(all_to_all_direct_basename,
             num_sites_arg(num_localities), this_site_arg(this_locality));
 
-    // test functionality based on immediate local result value
-    for (int i = 0; i != 1; ++i)
+
+    // do stuff with vector
+    std::vector<std::uint32_t> values(size);
+    std::iota(values.begin(), values.end(), 0);
+
+    // divide vector
+    std::vector<std::vector<std::uint32_t>> values_div(num_localities);
+        
+    for (std::size_t i = 0; i != num_localities; ++i)
     {
-        std::vector<std::uint32_t> values(2*num_localities);
-        std::fill(values.begin(), values.end(), this_locality);
-        if(this_locality == 0)
-        {
-            for (auto v : values)
+        std::vector<std::uint32_t> tmp(std::make_move_iterator(values.begin()+i*sub_size),
+                             std::make_move_iterator(values.begin()+(i+1)*sub_size)); // move;
+        values_div.push_back(tmp);
+    }
+    
+    if(1)//this_locality == 0)
+    {
+            for (auto va : values_div)
             {
-            char const* msg = "Locality {1} v: {2}\n";
-            hpx::util::format_to(hpx::cout, msg, this_locality, v)
-                << std::flush;
+                for (auto v : va)
+                {
+                char const* msg = "Locality {1} v: {2}\n";
+                hpx::util::format_to(hpx::cout, msg, this_locality, v)
+                    << std::flush;
+                }
             }
-        }
+    }
+    
 
-        hpx::future<std::vector<std::uint32_t>> overall_result = all_to_all(
-            all_to_all_direct_client, std::move(values), generation_arg(i + 1));
+    hpx::future<std::vector<std::vector<std::uint32_t>>> overall_result = all_to_all(
+        all_to_all_direct_client, std::move(values_div),generation_arg(0 + 1));
+    
 
-        std::vector<std::uint32_t> r = overall_result.get();
-        HPX_TEST_EQ(r.size(), num_localities);
+    std::vector<std::vector<std::uint32_t>> r = overall_result.get();
 
-        for (std::size_t j = 0; j != r.size(); ++j)
+    if(1)//this_locality == 0)
+    {
+        for (auto va : r)
         {
-            HPX_TEST_EQ(r[j], j);
-        }
-
-        if(this_locality == 0)
-        {
-            for (auto v : r)
+            std::cout << va.size() << " hello\n";
+            for (auto v : va)
             {
+                std::cout << "in\n";
             char const* msg = "Locality {1} r: {2}\n";
             hpx::util::format_to(hpx::cout, msg, this_locality, v)
                 << std::flush;
@@ -132,6 +127,7 @@ void test_multiple_use_with_generation()
         }
     }
 }
+
 
 int hpx_main()
 {
