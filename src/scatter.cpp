7b753ae89d6f3ab1bc79cd051eab9275a52ac5ed
Alexander Strack <strack.as@icloud.com>
2023-08-30 17:39:12 +0200

    working 2D forward FFT with HPX

--- src/scatter.cpp
+++ src/scatter.cpp
@@ -12,16 +12,12 @@
 #include <hpx/modules/collectives.hpp>
 #include <hpx/iostream.hpp>
 
-
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-
 // FFTW
 #include <fftw3.h>
-
-//using namespace hpx::collectives;
 
 using vector = std::vector<double, std::allocator<double>>;
 using split_vector = std::vector<vector>;
@@ -58,7 +54,7 @@ void test_multiple_use_with_generation()
     // holder for communication futures
     std::vector<hpx::shared_future<vector>> communication_tmp_futures(num_localities);
     // FFTW plans
-    unsigned FFTW_PLAN_FLAG = FFTW_ESTIMATE;
+    const unsigned FFTW_PLAN_FLAG = FFTW_ESTIMATE;
     std::vector<fftw_plan> plans_1D_r2c(n_x_local);
     std::vector<fftw_plan> plans_1D_c2c(n_y_local);
     ////////////////////////////////////////////////////////////////
@@ -114,7 +110,6 @@ void test_multiple_use_with_generation()
             vector tmp(std::make_move_iterator(values_vec[i].begin()+j*dim_c_y_part),
                                 std::make_move_iterator(values_vec[i].begin()+(j+1)*dim_c_y_part)); // move;
             values_div_vec[i][j] = std::move(tmp);
-
         });
     });
     ////////////////////////////////////////////////////////////////
@@ -169,37 +164,7 @@ void test_multiple_use_with_generation()
         fftw_execute(plans_1D_c2c[i]);
     });
     ////////////////////////////////////////////////////////////////
-    // print result
-    // sleep(this_locality);
-    // if (1)
-    // {
-    //     char const* msg = "\nLocality {1}:";
-    //     hpx::util::format_to(hpx::cout, msg, this_locality) << std::flush;
-    //     for (auto r5 : trans_values_vec)
-    //     {
-    //         char const* msg = "\n";
-    //         hpx::util::format_to(hpx::cout, msg) << std::flush;
-    //         std::uint32_t counter = 0;
-    //         for (auto v : r5)
-    //         {
-    //             if(counter%2 == 0)
-    //             {
-    //                 char const* msg = "({1} ";
-    //                 hpx::util::format_to(hpx::cout, msg, v) << std::flush;
-    //             }
-    //             else
-    //             {
-    //                 char const* msg = "{1}) ";
-    //                 hpx::util::format_to(hpx::cout, msg, v) << std::flush;
-    //             }
-    //             ++counter;
-    //         }
-    //     }
-    //     char const* msg2 = "\n";
-    //     hpx::util::format_to(hpx::cout, msg2) << std::flush;
-    // }
-    ////////////////////////////////////////////////////////////////
-    // divide trans value vector
+    // divide transposed value vector
     hpx::experimental::for_loop(hpx::execution::par, 0, n_y_local, [&](auto i)
     {
         hpx::experimental::for_loop(hpx::execution::par, 0, num_localities, [&](auto j)
@@ -210,41 +175,6 @@ void test_multiple_use_with_generation()
 
         });
     });
-
-    // sleep(this_locality);
-    // if (1)
-    // {
-    //     char const* msg = "\nLocality {1}:";
-    //     hpx::util::format_to(hpx::cout, msg, this_locality) << std::flush;
-    //     for (auto r4 :trans_values_div_vec)
-    //     {
-    //         char const* msg = "\n";
-    //         hpx::util::format_to(hpx::cout, msg) << std::flush;
-    //         for (auto r5 : r4)
-    //         {
-    //             char const* msg = "\n";
-    //             hpx::util::format_to(hpx::cout, msg) << std::flush;
-    //             std::uint32_t counter = 0;
-    //             for (auto v : r5)
-    //             {
-    //                 if(counter%2 == 0)
-    //                 {
-    //                     char const* msg = "({1} ";
-    //                     hpx::util::format_to(hpx::cout, msg, v) << std::flush;
-    //                 }
-    //                 else
-    //                 {
-    //                     char const* msg = "{1}) ";
-    //                     hpx::util::format_to(hpx::cout, msg, v) << std::flush;
-    //                 }
-    //                 ++counter;
-    //             }
-    //         }
-    //     }
-    //     char const* msg2 = "\n";
-    //     hpx::util::format_to(hpx::cout, msg2) << std::flush;
-    // }
-
     ////////////////////////////////////////////////////////////////
     // communication
     hpx::experimental::for_loop(hpx::execution::seq, 0, n_y_local, [&](auto i)
@@ -275,6 +205,118 @@ void test_multiple_use_with_generation()
         });
         ++generation_counter;
     });
+
+    ////////////////////////////////////////////////////////////////
+    // local transpose: change leading dimension from x to y
+    // optimize loops
+    hpx::experimental::for_loop(hpx::execution::par, 0, num_localities, [&](auto i)
+    {   
+        hpx::experimental::for_loop(hpx::execution::par, 0, n_y_local, [&](auto j)
+        {
+            hpx::experimental::for_loop(hpx::execution::par, 0, n_x_local, [&](auto k)
+            {
+                values_vec[k][j* num_localities*2 + 2*i] = communication_vec[i][j][2*k];
+                values_vec[k][j* num_localities*2 + 2*i+1] = communication_vec[i][j][2*k+1];
+            });
+        });    
+    });
+    ////////////////////////////////////////////////////////////////
+    // print result
+    sleep(this_locality);
+    if (1)
+    {
+        char const* msg = "\nLocality {1}:";
+        hpx::util::format_to(hpx::cout, msg, this_locality) << std::flush;
+        for (auto r5 : values_vec)
+        {
+            char const* msg = "\n";
+            hpx::util::format_to(hpx::cout, msg) << std::flush;
+            std::uint32_t counter = 0;
+            for (auto v : r5)
+            {
+                if(counter%2 == 0)
+                {
+                    char const* msg = "({1} ";
+                    hpx::util::format_to(hpx::cout, msg, v) << std::flush;
+                }
+                else
+                {
+                    char const* msg = "{1}) ";
+                    hpx::util::format_to(hpx::cout, msg, v) << std::flush;
+                }
+                ++counter;
+            }
+        }
+        char const* msg2 = "\n";
+        hpx::util::format_to(hpx::cout, msg2) << std::flush;
+    }
+    ////////////////////////////////////////////////////////////////
+    // TODO Backwards transform
+
+
+    ////////////////////////////////////////////////
+    // Cleanup
+    // FFTW cleanup
+    hpx::experimental::for_loop(hpx::execution::par, 0, n_x_local, [&](auto i)
+    {
+        fftw_destroy_plan(plans_1D_r2c[i]);
+    });
+    // forward step two: c2c in x-direction
+    hpx::experimental::for_loop(hpx::execution::par, 0, n_y_local, [&](auto i)
+    {
+        fftw_destroy_plan(plans_1D_c2c[i]);
+    });
+    fftw_cleanup();
+}
+
+int hpx_main()
+{
+    test_multiple_use_with_generation();
+
+    return hpx::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> const cfg = {"hpx.run_hpx_main!=1"};
+
+    hpx::init_params init_args;
+    init_args.cfg = cfg;
+
+    return hpx::init(argc, argv, init_args);
+}
+
+#endif
+    ////////////////////////////////////////////////////////////////
+    // print result
+    // sleep(this_locality);
+    // if (1)
+    // {
+    //     char const* msg = "\nLocality {1}:";
+    //     hpx::util::format_to(hpx::cout, msg, this_locality) << std::flush;
+    //     for (auto r5 : trans_values_vec)
+    //     {
+    //         char const* msg = "\n";
+    //         hpx::util::format_to(hpx::cout, msg) << std::flush;
+    //         std::uint32_t counter = 0;
+    //         for (auto v : r5)
+    //         {
+    //             if(counter%2 == 0)
+    //             {
+    //                 char const* msg = "({1} ";
+    //                 hpx::util::format_to(hpx::cout, msg, v) << std::flush;
+    //             }
+    //             else
+    //             {
+    //                 char const* msg = "{1}) ";
+    //                 hpx::util::format_to(hpx::cout, msg, v) << std::flush;
+    //             }
+    //             ++counter;
+    //         }
+    //     }
+    //     char const* msg2 = "\n";
+    //     hpx::util::format_to(hpx::cout, msg2) << std::flush;
+    // }
 
     // sleep(this_locality);
     // if (1)
@@ -310,131 +352,36 @@ void test_multiple_use_with_generation()
     //     hpx::util::format_to(hpx::cout, msg2) << std::flush;
     // }
 
-    ////////////////////////////////////////////////////////////////
-    // local transpose: change leading dimension from y to x
-    // optimize loops
-    hpx::experimental::for_loop(hpx::execution::par, 0, num_localities, [&](auto i)
-    {   
-        hpx::experimental::for_loop(hpx::execution::par, 0, n_y_local, [&](auto j)
-        {
-            hpx::experimental::for_loop(hpx::execution::par, 0, n_x_local, [&](auto k)
-            {
-                values_vec[k][j* num_localities*2 + 2*i] = communication_vec[i][j][2*k];
-                values_vec[k][j* num_localities*2 + 2*i+1] = communication_vec[i][j][2*k+1];
-            });
-        });    
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // // divide value vector
-    // hpx::experimental::for_loop(hpx::execution::par, 0, n_x_local, [&](auto i)
+    // sleep(this_locality);
+    // if (1)
     // {
-    //     hpx::experimental::for_loop(hpx::execution::par, 0, num_localities, [&](auto j)
+    //     char const* msg = "\nLocality {1}:";
+    //     hpx::util::format_to(hpx::cout, msg, this_locality) << std::flush;
+    //     for (auto r4 :trans_values_div_vec)
     //     {
-    //         vector tmp(std::make_move_iterator(values_vec[i].begin()+j*dim_c_y_part),
-    //                             std::make_move_iterator(values_vec[i].begin()+(j+1)*dim_c_y_part)); // move;
-    //         values_div_vec[i][j] = std::move(tmp);
-
-    //     });
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // print result
-    sleep(this_locality);
-    if (1)
-    {
-        char const* msg = "\nLocality {1}:";
-        hpx::util::format_to(hpx::cout, msg, this_locality) << std::flush;
-        for (auto r5 : values_vec)
-        {
-            char const* msg = "\n";
-            hpx::util::format_to(hpx::cout, msg) << std::flush;
-            std::uint32_t counter = 0;
-            for (auto v : r5)
-            {
-                if(counter%2 == 0)
-                {
-                    char const* msg = "({1} ";
-                    hpx::util::format_to(hpx::cout, msg, v) << std::flush;
-                }
-                else
-                {
-                    char const* msg = "{1}) ";
-                    hpx::util::format_to(hpx::cout, msg, v) << std::flush;
-                }
-                ++counter;
-            }
-        }
-        char const* msg2 = "\n";
-        hpx::util::format_to(hpx::cout, msg2) << std::flush;
-    }
-
-    ////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////
-    // Cleanup
-    // FFTW cleanup
-    hpx::experimental::for_loop(hpx::execution::par, 0, n_x_local, [&](auto i)
-    {
-        fftw_destroy_plan(plans_1D_r2c[i]);
-    });
-    // forward step two: c2c in x-direction
-    hpx::experimental::for_loop(hpx::execution::par, 0, n_y_local, [&](auto i)
-    {
-        fftw_destroy_plan(plans_1D_c2c[i]);
-    });
-    fftw_cleanup();
-
-
-
-
-
-
-
-
-}
-
-int hpx_main()
-{
-    test_multiple_use_with_generation();
-
-    return hpx::finalize();
-}
-
-int main(int argc, char* argv[])
-{
-    std::vector<std::string> const cfg = {"hpx.run_hpx_main!=1"};
-
-    hpx::init_params init_args;
-    init_args.cfg = cfg;
-
-    return hpx::init(argc, argv, init_args);
-}
-
-#endif
+    //         char const* msg = "\n";
+    //         hpx::util::format_to(hpx::cout, msg) << std::flush;
+    //         for (auto r5 : r4)
+    //         {
+    //             char const* msg = "\n";
+    //             hpx::util::format_to(hpx::cout, msg) << std::flush;
+    //             std::uint32_t counter = 0;
+    //             for (auto v : r5)
+    //             {
+    //                 if(counter%2 == 0)
+    //                 {
+    //                     char const* msg = "({1} ";
+    //                     hpx::util::format_to(hpx::cout, msg, v) << std::flush;
+    //                 }
+    //                 else
+    //                 {
+    //                     char const* msg = "{1}) ";
+    //                     hpx::util::format_to(hpx::cout, msg, v) << std::flush;
+    //                 }
+    //                 ++counter;
+    //             }
+    //         }
+    //     }
+    //     char const* msg2 = "\n";
+    //     hpx::util::format_to(hpx::cout, msg2) << std::flush;
+    // }
