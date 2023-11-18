eb674debcfa1c95ae0872b58f36332ddaf6ec107
Alexander Strack <strack.as@icloud.com>
2023-11-18 10:25:49 +0100

    buran: apex task graph

--- buran_bench_dist.sh
+++ buran_bench_dist.sh
@@ -10,7 +10,7 @@ set +x
 # Compute benchmark script from 2^start to 2^stop
 # POW_START=$1
 # POW_STOP=$2
-LOOP=5
+LOOP=1
 
 ################################################################################
 # Run benchmark script
@@ -99,46 +99,46 @@ do
     done
 done
 
-# scatter task agas
-srun -p buran -N 1 -t 1:00:00 --exclusive ./build/fft_hpx_task_agas --hpx:threads=$THREADS --nx=$BASE_SIZE  --ny=$BASE_SIZE  --run=scatter --header=true
-for (( j=1; j<$LOOP; j=j+1 ))
-do
-    srun -p buran -N 1 -t 1:00:00 --exclusive ./build/fft_hpx_task_agas --hpx:threads=$THREADS --nx=$BASE_SIZE  --ny=$BASE_SIZE  --run=scatter 
-done
-for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
-do
-    for (( j=0; j<$LOOP; j=j+1 ))
-    do
-        srun -p buran -N $(($i*$i)) -t 1:00:00 --exclusive ./build/fft_hpx_task_agas --hpx:threads=$THREADS --nx=$(($i*$BASE_SIZE))  --ny=$(($i*$BASE_SIZE))  --run=scatter 
-    done
-done
+# # scatter task agas
+# srun -p buran -N 1 -t 1:00:00 --exclusive ./build/fft_hpx_task_agas --hpx:threads=$THREADS --nx=$BASE_SIZE  --ny=$BASE_SIZE  --run=scatter --header=true
+# for (( j=1; j<$LOOP; j=j+1 ))
+# do
+#     srun -p buran -N 1 -t 1:00:00 --exclusive ./build/fft_hpx_task_agas --hpx:threads=$THREADS --nx=$BASE_SIZE  --ny=$BASE_SIZE  --run=scatter 
+# done
+# for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
+# do
+#     for (( j=0; j<$LOOP; j=j+1 ))
+#     do
+#         srun -p buran -N $(($i*$i)) -t 1:00:00 --exclusive ./build/fft_hpx_task_agas --hpx:threads=$THREADS --nx=$(($i*$BASE_SIZE))  --ny=$(($i*$BASE_SIZE))  --run=scatter 
+#     done
+# done
 
-# MPI + OpenMP
-srun -p buran -N 1 -t 1:00:00 --exclusive -n 1 ./build/fftw_mpi_omp $THREADS $BASE_SIZE $BASE_SIZE estimate 1
-for (( j=1; j<$LOOP; j=j+1 ))
-do
-    srun -p buran -N 1 -t 1:00:00 --exclusive -n 1 ./build/fftw_mpi_omp $THREADS $BASE_SIZE $BASE_SIZE estimate 0
-done
-for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
-do
-    for (( j=0; j<$LOOP; j=j+1 ))
-    do
-        srun -p buran -N $(($i*$i)) -t 1:00:00 --exclusive -n $(($i*$i)) ./build/fftw_mpi_omp $THREADS $(($i*$BASE_SIZE)) $(($i*$BASE_SIZE)) estimate 0 --use-hwthread-cpus
-    done
-done
+# # MPI + OpenMP
+# srun -p buran -N 1 -t 1:00:00 --exclusive -n 1 ./build/fftw_mpi_omp $THREADS $BASE_SIZE $BASE_SIZE estimate 1
+# for (( j=1; j<$LOOP; j=j+1 ))
+# do
+#     srun -p buran -N 1 -t 1:00:00 --exclusive -n 1 ./build/fftw_mpi_omp $THREADS $BASE_SIZE $BASE_SIZE estimate 0
+# done
+# for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
+# do
+#     for (( j=0; j<$LOOP; j=j+1 ))
+#     do
+#         srun -p buran -N $(($i*$i)) -t 1:00:00 --exclusive -n $(($i*$i)) ./build/fftw_mpi_omp $THREADS $(($i*$BASE_SIZE)) $(($i*$BASE_SIZE)) estimate 0 --use-hwthread-cpus
+#     done
+# done
 
-# MPI total
-srun -p buran -N 1 -t 1:00:00 --exclusive -n $THREADS ./build/fftw_mpi_omp 1 $BASE_SIZE $BASE_SIZE estimate 1
-for (( j=1; j<$LOOP; j=j+1 ))
-do
-    srun -p buran -N 1 -t 1:00:00 --exclusive -n $THREADS ./build/fftw_mpi_omp 1 $BASE_SIZE $BASE_SIZE estimate 0
-done
-for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
-do
-    for (( j=0; j<$LOOP; j=j+1 ))
-    do
-        srun -p buran -N $(($i*$i)) -t 1:00:00 --exclusive -n $(($i*$i*$THREADS)) ./build/fftw_mpi_omp 1 $(($i*$BASE_SIZE)) $(($i*$BASE_SIZE)) estimate 0 --use-hwthread-cpus
-    done
-done
+# # MPI total
+# srun -p buran -N 1 -t 1:00:00 --exclusive -n $THREADS ./build/fftw_mpi_omp 1 $BASE_SIZE $BASE_SIZE estimate 1
+# for (( j=1; j<$LOOP; j=j+1 ))
+# do
+#     srun -p buran -N 1 -t 1:00:00 --exclusive -n $THREADS ./build/fftw_mpi_omp 1 $BASE_SIZE $BASE_SIZE estimate 0
+# done
+# for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
+# do
+#     for (( j=0; j<$LOOP; j=j+1 ))
+#     do
+#         srun -p buran -N $(($i*$i)) -t 1:00:00 --exclusive -n $(($i*$i*$THREADS)) ./build/fftw_mpi_omp 1 $(($i*$BASE_SIZE)) $(($i*$BASE_SIZE)) estimate 0 --use-hwthread-cpus
+#     done
+# done
 
 ###################################################################################################
