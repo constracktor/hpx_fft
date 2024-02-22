d9bf216ed17afdb0f96e1e655f36418aa113bb6e
Alexander Strack <strack.as@icloud.com>
2024-02-22 15:19:08 -0500

    minimum working example

--- README.md
+++ README.md
@@ -6,10 +6,6 @@ This repo is currently under active developement.
 - /src contains different shared-memory and distributed implementations 
 of the same 2D FFT example
 
-- /plot/data contains runtimes for a 16 node cluster (buran) and a 128 core dual socket system (ipvs-epyc2)
-
-- /plot/figures contains weak and strong scaling runtimes, distributions and more
-
 
 ## Contact
 
--- compile.sh
+++ compile.sh
@@ -10,34 +10,22 @@ set +x
 # working directory
 export ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/.."
 # export paths
-if [[ "$1" == "epyc2" ]]
-then
-    # epyc2
-    export HPXSC_ROOT="${ROOT}/hpxsc_installations/hpx_apex_epyc2_v.1.9.1"
-    export FFTW_SEQ_DIR="${ROOT}/fft_installations/fftw_seq/install/lib/" 
-elif [[ "$1" == "buran" ]]
+if [[ "$1" == "buran" ]]
 then
     # buran
     export HPXSC_ROOT="${ROOT}/hpxsc_installations/hpx_1.9.1_mpi_gcc_11.2.1"
-    #export HPXSC_ROOT="${ROOT}/hpxsc_installations/hpx_1.9.1_lci_gcc_11.2.1"
-    export FFTW_SEQ_DIR="${HPXSC_ROOT}/build/fftw/lib64/"
 elif [[ "$1" == "medusa" ]]
 then
     # medusa
     export HPXSC_ROOT="${ROOT}/hpxsc_installations/hpx_1.9.1_mpi_gcc_11.2.1_medusa"
-    export FFTW_SEQ_DIR="${HPXSC_ROOT}/build/fftw/lib64/"
 else
-  echo 'Please specify system: "epyc2" or "buran" or "medusa"'
+  echo 'Please specify system: "buran" or "medusa"'
   exit 1
 fi
-export CMAKE_COMMAND=${HPXSC_ROOT}/build/cmake/bin/cmake
+
+export FFTW_SEQ_DIR="${HPXSC_ROOT}/build/fftw/lib64/"
+export CMAKE_COMMAND=cmake
 export HPX_DIR=${HPXSC_ROOT}/build/hpx/build/lib
-export CXX=${HPXSC_ROOT}/build/openmpi/bin/mpic++ 
-# FFTW paths
-export FFTW_TH_DIR="$ROOT/fft_installations/fftw_threads_mpi/install/lib"
-export FFTW_OMP_DIR="$ROOT/fft_installations/fftw_omp_mpi/install/lib"
-export FFTW_HPX_DIR="$ROOT/fft_installations/fftw_hpx/install/lib"
-export PKG_CONFIG_PATH="$FFTW_OMP_DIR/pkgconfig":$PKG_CONFIG_PATH
 
 ################################################################################
 # Compilation
