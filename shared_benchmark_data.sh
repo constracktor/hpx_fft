11a462cdc371575618b07cc4b01981edf49fe43f
Alexander Strack <strack.as@icloud.com>
2024-02-22 16:08:34 -0500

    adaptive basesize

--- shared_benchmark_data.sh
+++ shared_benchmark_data.sh
@@ -6,12 +6,13 @@
 mkdir result
 mkdir plans
 LOOP=1
-BASE_SIZE=1024
+BASE_SIZE_X=1024
+BASE_SIZE_Y=2048
 FFTW_PLAN=estimate
 #FFTW_PLAN=measure
 # Compute benchmark script from 2^start to 2^stop * BASE_SIZE
 POW_START=1
-POW_STOP=5
+POW_STOP=4
 THREADS=40
 PARTITION=medusa
 # get run command
@@ -28,15 +29,15 @@ fi
 ###############################
 # HPX loop shared
 # data scaling
-$COMMAND ./build/fft_hpx_loop_shared --hpx:threads=$THREADS --nx=$BASE_SIZE --ny=$BASE_SIZE --run=par --header=true --plan=$FFTW_PLAN
+$COMMAND ./build/fft_hpx_loop_shared --hpx:threads=$THREADS --nx=$BASE_SIZE_X --ny=$BASE_SIZE_Y --run=par --header=true --plan=$FFTW_PLAN
 for (( j=1; j<$LOOP; j=j+1 ))
 do
-    $COMMAND ./build/fft_hpx_loop_shared --hpx:threads=$THREADS --nx=$BASE_SIZE --ny=$BASE_SIZE --run=par --plan=$FFTW_PLAN
+    $COMMAND ./build/fft_hpx_loop_shared --hpx:threads=$THREADS --nx=$BASE_SIZE_X --ny=$BASE_SIZE_Y --run=par --plan=$FFTW_PLAN
 done
 for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
 do
     for (( j=0; j<$LOOP; j=j+1 ))
     do
-        $COMMAND ./build/fft_hpx_loop_shared --hpx:threads=$THREADS --nx=$((i * $BASE_SIZE)) --ny=$(($i * $BASE_SIZE)) --run=par --plan=$FFTW_PLAN
+        $COMMAND ./build/fft_hpx_loop_shared --hpx:threads=$THREADS --nx=$((i * $BASE_SIZE_X)) --ny=$(($i * $BASE_SIZE_Y)) --run=par --plan=$FFTW_PLAN
     done
 done
