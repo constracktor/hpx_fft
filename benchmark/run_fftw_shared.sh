4293064be3fc8401a1e8a84ac00a722c8649a3ec
Alexander Strack <strack.as@icloud.com>
2024-02-23 21:53:53 -0500

    add fftw backend script

--- benchmark/run_fftw_shared.sh
+++ benchmark/run_fftw_shared.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=hpx_fft_job          # Job name
-#SBATCH --output=hpx_fft_job.log        # Standard output and error log
-#SBATCH --mail-type=NONE                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --job-name=fftw_backend_job     # Job name
+#SBATCH --output=fftw_backend_job.log   # Standard output and error log
+#SBATCH --mail-type=NONE                # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=alexander.strack@ipvs.uni-stuttgart.de       # Where to send mail	
 #SBATCH --time=01:00:00                 # Time limit hrs:min:sec
 #SBATCH --exclusive                     # Exclusive ressource access
@@ -11,38 +11,35 @@
 #SBATCH --cpus-per-task=32              # Number of cores per MPI rank 
 
 # Benchmark script for shared-memory strong scaling
+# $1: Executable name
+# $2: FFTW planning flag (estimate/measure)
 # Parameters
 LOOP=1
 POW_START=1
 POW_STOP=5
 BASE_SIZE=16384
-FFTW_PLAN=estimate
-#FFTW_PLAN=measure
 # Get run command
-COMMAND="srun -N 1 -n 1 -c 1"
+COMMAND="FFTW3_HPX_NTHREADS=1 srun -N 1 -n 1 -c 1"
 EXECUTABLE="../build/$1"
-ARGUMENTS="--nx=$BASE_SIZE --ny=$BASE_SIZE --run=par --plan=$FFTW_PLAN"
+ARGUMENTS="$BASE_SIZE $BASE_SIZE $2"
 # Log Info
 pwd; hostname; date
 # Create directories to store data
 mkdir result
 mkdir plans
 # Strong scaling loop from 2^pow_start to 2^pow_stop
-$COMMAND $EXECUTABLE $ARGUMENTS --header=true 
+$COMMAND $EXECUTABLE 1 $ARGUMENTS 1 
 for (( j=1; j<$LOOP; j=j+1 ))
 do
-    $COMMAND $EXECUTABLE $ARGUMENTS
+    $COMMAND $EXECUTABLE 1 $ARGUMENTS 0
 done
 for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
 do
-    COMMAND="srun -N 1 -n 1 -c $i"
+    COMMAND="FFTW3_HPX_NTHREADS=$i srun -N 1 -n 1 -c $i"
     for (( j=0; j<$LOOP; j=j+1 ))
     do
-        $COMMAND $EXECUTABLE $ARGUMENTS
+        $COMMAND $EXECUTABLE $i $ARGUMENTS 0
     done
 done
 # Log Info
 date
-
-
-
