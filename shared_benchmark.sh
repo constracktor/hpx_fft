1617e18596b17748a62236253850656e8aa7271d
Alexander Strack <strack.as@icloud.com>
2024-02-24 00:03:20 -0500

    fix scripts for multiple servers

--- shared_benchmark.sh
+++ shared_benchmark.sh
@@ -6,35 +6,36 @@
 if [[ "$2" == "buran" ]]
 then
     PARTITION=buran
-    THREADS=32
+    THREAD_POW=5
 elif [[ "$2" == "medusa" ]]
 then
     PARTITION=medusa
-    THREADS=32
+    THREAD_POW=5
 elif [[ "$2" == "epyc1" ]]
 then
     PARTITION=ipvs-epcy1
-    THREADS=128
+    THREAD_POW=7
 else
   echo 'Please specify benchmark: "buran" or "medusa" or "epyc1"'
   exit 1
 fi
 FFTW_PLAN=$1
+THREADS=$((2**$THREAD_POW))
 cd benchmark
 # HPX implementations
 # shared only
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_loop_shared $FFTW_PLAN
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_sync_shared $FFTW_PLAN
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_shared $FFTW_PLAN
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_naive_shared $FFTW_PLAN
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_agas_shared $FFTW_PLAN
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_loop_shared $FFTW_PLAN $THREAD_POW
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_sync_shared $FFTW_PLAN $THREAD_POW
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_shared $FFTW_PLAN $THREAD_POW
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_naive_shared $FFTW_PLAN $THREAD_POW
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_agas_shared $FFTW_PLAN $THREAD_POW
 # distributed possible
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_loop $FFTW_PLAN
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_agas $FFTW_PLAN
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_loop $FFTW_PLAN $THREAD_POW
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh fft_hpx_task_agas $FFTW_PLAN $THREAD_POW
 # FFTW backends
 # shared only
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh fftw_hpx $FFTW_PLAN
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh fftw_hpx $FFTW_PLAN $THREAD_POW
 # distributed possible
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh fftw_mpi_threads $FFTW_PLAN
-sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh fftw_mpi_omp $FFTW_PLAN
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh fftw_mpi_THREAD_POW $FFTW_PLAN $THREAD_POW
+sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh fftw_mpi_omp $FFTW_PLAN $THREAD_POW
 
