243966b9087c0dd69315134e8823b9d9fe8e3a3f
Alexander Strack <strack.as@icloud.com>
2024-02-23 23:10:03 -0500

    add fftw dist script

--- benchmark/run_fftw_dist.sh
+++ benchmark/run_fftw_dist.sh
@@ -6,20 +6,22 @@
 #SBATCH --time=01:00:00                 # Time limit hrs:min:sec
 #SBATCH --exclusive                     # Exclusive ressource access
 #SBATCH --partition=buran               # Name of partition
-#SBATCH --nodes=1                       # Number of nodes
-#SBATCH --ntasks=1                      # Number of MPI ranks
-#SBATCH --cpus-per-task=32              # Number of cores per MPI rank 
+#SBATCH --nodes=16                      # Number of nodes
+#SBATCH --ntasks=16                     # Number of MPI ranks
+#SBATCH --cpus-per-task=48              # Number of cores per MPI rank 
 
-# Benchmark script for shared memory strong scaling
+# Benchmark script for distributed memory strong scaling
 # $1: Executable name
 # $2: FFTW planning flag (estimate/measure)
 # Parameters
 LOOP=1
 POW_START=1
-POW_STOP=5
+POW_STOP=4
 BASE_SIZE=16384
+THREADS=48
+PARTITION=buran
 # Get run command
-COMMAND="srun -N 1 -n 1 -c 1"
+COMMAND="srun -p $PARTITION -N 1 -n 1 -c $THREADS"
 EXECUTABLE="../build/$1"
 ARGUMENTS="$BASE_SIZE $BASE_SIZE $2"
 # Log Info
@@ -27,18 +29,18 @@ pwd; hostname; date
 # Create directories to store data
 mkdir result
 mkdir plans
-# Strong scaling loop from 2^pow_start to 2^pow_stop cores
-FFTW3_HPX_NTHREADS=1 $COMMAND $EXECUTABLE 1 $ARGUMENTS 1 
+# Strong scaling loop from 2^pow_start to 2^pow_stop nodes
+$COMMAND $EXECUTABLE $THREADS $ARGUMENTS 1
 for (( j=1; j<$LOOP; j=j+1 ))
 do
-    FFTW3_HPX_NTHREADS=1 $COMMAND $EXECUTABLE 1 $ARGUMENTS 0
+    $COMMAND $EXECUTABLE $THREADS $ARGUMENTS 0
 done
 for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
 do
-    COMMAND="srun -N 1 -n 1 -c $i"
+    COMMAND="srun -p $PARTITION -N $i -n $i -c $THREADS"
     for (( j=0; j<$LOOP; j=j+1 ))
     do
-        FFTW3_HPX_NTHREADS=$i $COMMAND $EXECUTABLE $i $ARGUMENTS 0
+        $COMMAND $EXECUTABLE $THREADS $ARGUMENTS 0
     done
 done
 # Log Info
