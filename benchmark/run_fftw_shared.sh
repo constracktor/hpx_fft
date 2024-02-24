f809177f6a5b877cc87036f1df37adb927e2da08
Alexander Strack <strack.as@icloud.com>
2024-02-23 22:24:12 -0500

    fix fftw backend script

--- benchmark/run_fftw_shared.sh
+++ benchmark/run_fftw_shared.sh
@@ -19,7 +19,7 @@ POW_START=1
 POW_STOP=5
 BASE_SIZE=16384
 # Get run command
-COMMAND="FFTW3_HPX_NTHREADS=1 srun -N 1 -n 1 -c 1"
+COMMAND="srun -N 1 -n 1 -c 1"
 EXECUTABLE="../build/$1"
 ARGUMENTS="$BASE_SIZE $BASE_SIZE $2"
 # Log Info
@@ -28,17 +28,17 @@ pwd; hostname; date
 mkdir result
 mkdir plans
 # Strong scaling loop from 2^pow_start to 2^pow_stop
-$COMMAND $EXECUTABLE 1 $ARGUMENTS 1 
+FFTW3_HPX_NTHREADS=1 $COMMAND $EXECUTABLE 1 $ARGUMENTS 1 
 for (( j=1; j<$LOOP; j=j+1 ))
 do
-    $COMMAND $EXECUTABLE 1 $ARGUMENTS 0
+    FFTW3_HPX_NTHREADS=1 $COMMAND $EXECUTABLE 1 $ARGUMENTS 0
 done
 for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
 do
-    COMMAND="FFTW3_HPX_NTHREADS=$i srun -N 1 -n 1 -c $i"
+    COMMAND="srun -N 1 -n 1 -c $i"
     for (( j=0; j<$LOOP; j=j+1 ))
     do
-        $COMMAND $EXECUTABLE $i $ARGUMENTS 0
+        FFTW3_HPX_NTHREADS=$i $COMMAND $EXECUTABLE $i $ARGUMENTS 0
     done
 done
 # Log Info
