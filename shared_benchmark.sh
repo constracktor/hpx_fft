b2747cef33037b2d3bc922c40b1cb51ae93f33fe
Alexander Strack <strack.as@icloud.com>
2024-02-23 21:53:53 -0500

    add fftw backend script

--- shared_benchmark.sh
+++ shared_benchmark.sh
@@ -4,64 +4,18 @@
 cd scripts
 # HPX implementations
 # shared
-sbatch run_hpx_shared.sh fft_hpx_loop_shared
-sbatch run_hpx_shared.sh fft_hpx_task_sync_shared
-sbatch run_hpx_shared.sh fft_hpx_task_shared
-sbatch run_hpx_shared.sh fft_hpx_task_naive_shared
-sbatch run_hpx_shared.sh fft_hpx_task_agas_shared
+sbatch run_hpx_shared.sh fft_hpx_loop_shared estimate
+sbatch run_hpx_shared.sh fft_hpx_task_sync_shared estimate
+sbatch run_hpx_shared.sh fft_hpx_task_shared estimate
+sbatch run_hpx_shared.sh fft_hpx_task_naive_shared estimate
+sbatch run_hpx_shared.sh fft_hpx_task_agas_shared estimate
 # distributed
-sbatch run_hpx_shared.sh fft_hpx_loop
-sbatch run_hpx_shared.sh fft_hpx_task_agas
+sbatch run_hpx_shared.sh fft_hpx_loop estimate
+sbatch run_hpx_shared.sh fft_hpx_task_agas estimate
 # FFTW backends
-# # Threads
-# $COMMAND ./build/fftw_mpi_threads 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 1
-# for (( j=1; j<$LOOP; j=j+1 ))
-# do
-#     $COMMAND ./build/fftw_mpi_threads 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0
-# done
-# for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
-# do
-#     for (( j=0; j<$LOOP; j=j+1 ))
-#     do
-#         $COMMAND ./build/fftw_mpi_threads $i $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0 
-#     done
-# done
-# # OpenMP
-# $COMMAND ./build/fftw_mpi_omp 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 1
-# for (( j=1; j<$LOOP; j=j+1 ))
-# do
-#     $COMMAND ./build/fftw_mpi_omp 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0
-# done
-# for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
-# do
-#     for (( j=0; j<$LOOP; j=j+1 ))
-#     do
-#         $COMMAND ./build/fftw_mpi_omp $i $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0 
-#     done
-# done
-# # MPI
-# $COMMAND -n 1 ./build/fftw_mpi_threads 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 1
-# for (( j=1; j<$LOOP; j=j+1 ))
-# do
-#     $COMMAND -n 1 ./build/fftw_mpi_threads 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0
-# done
-# for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
-# do
-#     for (( j=0; j<$LOOP; j=j+1 ))
-#     do
-#         $COMMAND -n $i ./build/fftw_mpi_threads 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0 
-#     done
-# done
-# # HPX
-# FFTW3_HPX_NTHREADS=1 $COMMAND ./build/fftw_hpx 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 1
-# for (( j=1; j<$LOOP; j=j+1 ))
-# do
-#     FFTW3_HPX_NTHREADS=1 $COMMAND ./build/fftw_hpx 1 $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0
-# done
-# for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
-# do
-#     for (( j=0; j<$LOOP; j=j+1 ))
-#     do
-#         FFTW3_HPX_NTHREADS=$i $COMMAND ./build/fftw_hpx $i  $BASE_SIZE $BASE_SIZE $FFTW_PLAN 0 
-#     done
-# done
+# shared
+sbatch run_fftw_shared.sh fftw_hpx estimate
+# distributed
+sbatch run_fftw_shared.sh fftw_mpi_threads estimate
+sbatch run_fftw_shared.sh fftw_mpi_omp estimate
+
