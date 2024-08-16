978d0416302b3bf72e56bc071f50372e7fa7ff93
Alexander Strack <alex@rostam1.rostam.cct.lsu.edu>
2024-08-16 10:38:04 -0500

    Add new install hpx_shmem script

--- hpx_installations/install_hpx_shmem.sh
+++ hpx_installations/install_hpx_shmem.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # This script installs HPX with experimental OpenSHMEM parcelport
-
-# MODIFY: Adjust required modules
-module load gcc/12.3.0 boost hwloc/2.9.1 openmpi
+module load llvm/18.1.5 boost hwloc openmpi
 # structure
-export ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/hpx_dev_shmem"
+export ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 export DIR_SRC="$ROOT/src"
 export DIR_BUILD="$ROOT/build"
 export DIR_INSTALL="$ROOT/install"
@@ -12,13 +10,25 @@ export DIR_INSTALL="$ROOT/install"
 mkdir -p $DIR_SRC
 cd $DIR_SRC
 export DOWNLOAD_URL="https://github.com/ct-clmsn/hpx.git"
+#export DOWNLOAD_URL="https://github.com/constracktor/hpx-shmem.git"
 git clone ${DOWNLOAD_URL} .
 git checkout openshmem-parcelport
 # build
 mkdir -p $DIR_BUILD
 cd $DIR_BUILD
-# MODIFY: Adjust OpenSHMEM conduit
-cmake -DCMAKE_CXX_COMPILER=oshc++ -DHPX_WITH_PARCELPORT_OPENSHMEM_CONDUIT=mpi -DCMAKE_C_FLAGS='-DOSHMEM_HAVE_C11' -DHPX_WITH_MALLOC=tcmalloc -DHPX_WITH_PARCELPORT_TCP=OFF -DHPX_WITH_PARCELPORT_OPENSHMEM=ON  -DHPX_WITH_FETCH_ASIO=ON -DHPX_WITH_EXAMPLES=ON -DHPX_WITH_TESTS=OFF -DHPX_WITH_CXX_STANDARD=20 -DCMAKE_INSTALL_PREFIX=$DIR_INSTALL -DHPX_HAVE_NETWORKING=ON ../src
+cmake -DHPX_WITH_MALLOC=tcmalloc \
+      -DCMAKE_C_COMPILER=clang \
+      -DCMAKE_CXX_COMPILER=clang++ \
+      -DCMAKE_INSTALL_PREFIX=$DIR_INSTALL \
+      -DHPX_WITH_NETWORKING=ON \
+      -DHPX_WITH_PARCELPORT_TCP=OFF \
+      -DHPX_WITH_PARCELPORT_MPI=OFF \
+      -DHPX_WITH_FETCH_OPENSHMEM=OFF \
+      -DHPX_WITH_PARCELPORT_OPENSHMEM=ON \
+      -DHPX_WITH_PARCELPORT_OPENSHMEM_CONDUIT=mpi \
+      -DHPX_WITH_CXX_STANDARD=20 \
+      -DHPX_WITH_FETCH_ASIO=ON ../src
 make -j $(grep -c ^processor /proc/cpuinfo)
 # install
 make install
+# Run command: salloc -N 2 -p buran oshrun -n 2 ./hello_world_distributed |& less
